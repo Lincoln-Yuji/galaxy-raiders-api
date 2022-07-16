d81e41693ccc8a40cf984114c10f25bb715386be
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2022-07-16 15:43:18 -0300

    Iplemente as classes Point2D e Vector2D
    Signed-off-by: Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>

--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -5,62 +5,67 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties("unit", "normal", "degree", "magnitude")
 data class Vector2D(val dx: Double, val dy: Double) {
+
   override fun toString(): String {
     return "Vector2D(dx=$dx, dy=$dy)"
   }
 
   val magnitude: Double
-    get() = INVALID_DOUBLE
+    get() = Math.sqrt(dx * dx + dy * dy)
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get() = if (dy < 0.0) -1 * Math.acos(dx / magnitude) else Math.acos(dx / magnitude)
 
   val degree: Double
-    get() = INVALID_DOUBLE
+    get() = Math.toDegrees(radiant)
 
   val unit: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(dx / magnitude, dy / magnitude)
 
   val normal: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(dy / magnitude, -dx / magnitude)
 
   operator fun times(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx * scalar, dy * scalar)
   }
 
   operator fun div(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx / scalar, dy / scalar)
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    return dx * v.dx + dy * v.dy
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx + v.dx, dy + v.dy)
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(p.x + dx, p.y + dy)
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(-dx, -dy)
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx - v.dx, dy - v.dy)
   }
 
   fun scalarProject(target: Vector2D): Double {
-    return INVALID_DOUBLE
+    return this.times(target) / target.magnitude
   }
 
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val out: Vector2D = target.times(this.times(target) / target.times(target))
+    //val x: Int = 1000000
+    //val x_double: Double = 1000000.0
+    //return Vector2D(Math.round(out.dx * x) / x_double, Math.round(out.dy * x) / x_double)
+    return out
   }
 }
 
 operator fun Double.times(v: Vector2D): Vector2D {
-  return INVALID_VECTOR
+  return Vector2D(this * v.dx, this * v.dy)
 }
